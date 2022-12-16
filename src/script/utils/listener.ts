df801d31307b6981aa6229a758bb52a4d853e4c0
Shama8nchez <shamanouski@gmail.com>
2022-12-16 11:49:31 +0300

    feat: add dual range

--- src/script/utils/listener.ts
+++ src/script/utils/listener.ts
@@ -1,7 +1,12 @@
 import { showList, showGrid } from './actions';
+import { controlFromSlider, controlToSlider, setToggleAccessible } from './fromtofilter';
 
 const listView = <Element>document.querySelector('.goods__view_list');
 const gridView = <Element>document.querySelector('.goods__view_grid');
+const fromPrice = <HTMLInputElement>document.querySelector('#fromPrice');
+const toPrice = <HTMLInputElement>document.querySelector('#toPrice');
+const minPrice = <Element>document.querySelector('#minPrice');
+const maxPrice = <Element>document.querySelector('#maxPrice');
 
 export function listener() {
   document.body.addEventListener('click', (e) => {
@@ -12,4 +17,9 @@ export function listener() {
       showGrid();
     }
   });
+
+  setToggleAccessible(toPrice);
+
+  fromPrice.oninput = () => controlFromSlider(fromPrice, toPrice, minPrice);
+  toPrice.oninput = () => controlToSlider(fromPrice, toPrice, maxPrice);
 }
