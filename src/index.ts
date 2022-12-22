c3efaf5f288101fc96d267a11508bfc46f746bac
Yahor Litavar <sergioegor21@gmail.com>
2022-12-22 19:47:45 +0300

    feat: add local storage for cart

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './styles/style.scss';
 import { headerListener } from './script/utils/cart/cartListener';
 import { location } from './script/utils/router';
+import cart from './script/utils/cart/cart';
 
 location();
 headerListener();
+cart.setFromLocalStorage();
