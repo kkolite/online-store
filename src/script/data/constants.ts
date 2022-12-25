b9b64dbd6a42083c64b11357845c5cb5a2df2f2b
Shama8nchez <shamanouski@gmail.com>
2022-12-25 17:48:11 +0300

    feat: add pagination

--- src/script/data/constants.ts
+++ src/script/data/constants.ts
@@ -1,4 +1,5 @@
 export const CART_LIMIT = 3;
+export const MAX_ITEMS_PER_PAGE = 20;
 export const ERROR_TEXT = 'Error!';
 export const ERROR_PAYMENT_TEXT = 'Error! Please, check input fields';
 export const SUCSESS_PAYMENT_TEXT = 'Success! You will be redirected to main page in 5 seconds.';
