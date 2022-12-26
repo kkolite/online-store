d1a585188b939b95531a71d5671d8d72a159d1b1
Yahor Litavar <sergioegor21@gmail.com>
2022-12-26 18:02:12 +0300

    fix: add email in info validate test

--- src/tests/infoValiation.test.ts
+++ src/tests/infoValiation.test.ts
@@ -1,8 +1,9 @@
-import { validateAdress, validateName, validateTel } from "../script/utils/Payment/infoValidation";
+import { validateAdress, validateEMail, validateName, validateTel } from "../script/utils/Payment/infoValidation";
 
 document.body.innerHTML = `<div class="form__name_error"></div>
 <div class="form__adress_error"></div>
-<div class="form__tel_error"></div>`;
+<div class="form__tel_error"></div>;
+<div class="form__email_error"></div>`;
 
 describe('Name Validation', () => {
     it('should correct validate name', () => {
@@ -45,6 +46,21 @@ describe('Tel Validation', () => {
     });
     it('should return false if incorrect length of tel', () => {
         const result = validateTel('+37533')
+        expect(result).toEqual(false)
+    });
+})
+
+describe('EMail Validation', () => {
+    it('should correct validate email', () => {
+        const result = validateEMail('name@host.com')
+        expect(result).toEqual(true)
+    });
+    it('should return false if no @', () => {
+        const result = validateEMail('fakeemail.com')
+        expect(result).toEqual(false)
+    });
+    it('should return false if incorrect length of any part', () => {
+        const result = validateEMail('email@')
         expect(result).toEqual(false)
     });
 })
