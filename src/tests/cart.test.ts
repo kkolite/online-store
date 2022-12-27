5eff7f9bbdfd1141f09d8bfa5cd5993f763ce123
Yahor Litavar <sergioegor21@gmail.com>
2022-12-27 12:51:45 +0300

    feat: create query

--- src/tests/cart.test.ts
+++ src/tests/cart.test.ts
@@ -6,16 +6,16 @@ const stubCart: IGoods[] = [data[0], data[2], data[5]];
 const realCart = cart.cartArr;
 
 beforeEach(() => {
-    cart.cartArr = stubCart;
-})
-  
+  cart.cartArr = stubCart;
+});
+
 afterEach(() => {
-    cart.cartArr = realCart;
-})
-  
-  describe('Cart array length', () => {
-    it('should return the length 3', () => {
-      const result = cart.cartLength();
-      expect(result).toEqual(3)
-    })
-  })
+  cart.cartArr = realCart;
+});
+
+describe('Cart array length', () => {
+  it('should return the length 3', () => {
+    const result = cart.cartLength();
+    expect(result).toEqual(3);
+  });
+});
--- src/tests/paymentValidation.test.ts
+++ src/tests/paymentValidation.test.ts
@@ -1,46 +1,46 @@
-import { validateCard, validateCVC, validateDate } from "../script/utils/Payment/payValidation";
+import { validateCard, validateCVC, validateDate } from '../script/utils/Payment/payValidation';
 
 document.body.innerHTML = `<div class="form__card_error"></div>
 <div class="form__date_error"></div>
 <div class="form__cvc_error"></div>`;
 
 describe('Card Validation', () => {
-    it('should correct validate card', () => {
-        const result = validateCard('4444 4444 4444 4444')
-        expect(result).toEqual(true)
-    });
-    it('should return false if short card length', () => {
-        const result = validateCard('4255 2533 2355 25')
-        expect(result).toEqual(false)
-    });
-})
+  it('should correct validate card', () => {
+    const result = validateCard('4444 4444 4444 4444');
+    expect(result).toEqual(true);
+  });
+  it('should return false if short card length', () => {
+    const result = validateCard('4255 2533 2355 25');
+    expect(result).toEqual(false);
+  });
+});
 
 describe('Date Validation', () => {
-    it('should correct validate date', () => {
-        const result = validateDate('12 / 24')
-        expect(result).toEqual(true)
-    });
-    it('should return false if old', () => {
-        const result = validateDate('12 / 21')
-        expect(result).toEqual(false)
-    });
-    it('should return false if incorrect month', () => {
-        const result = validateDate('24 / 24')
-        expect(result).toEqual(false)
-    });
-})
+  it('should correct validate date', () => {
+    const result = validateDate('12 / 24');
+    expect(result).toEqual(true);
+  });
+  it('should return false if old', () => {
+    const result = validateDate('12 / 21');
+    expect(result).toEqual(false);
+  });
+  it('should return false if incorrect month', () => {
+    const result = validateDate('24 / 24');
+    expect(result).toEqual(false);
+  });
+});
 
 describe('CVC Validation', () => {
-    it('should correct validate CVC', () => {
-        const result = validateCVC('111')
-        expect(result).toEqual(true)
-    });
-    it('should return false if short', () => {
-        const result = validateCVC('87')
-        expect(result).toEqual(false)
-    });
-    it('should return false if big', () => {
-        const result = validateCVC('123456789')
-        expect(result).toEqual(false)
-    });
-})
+  it('should correct validate CVC', () => {
+    const result = validateCVC('111');
+    expect(result).toEqual(true);
+  });
+  it('should return false if short', () => {
+    const result = validateCVC('87');
+    expect(result).toEqual(false);
+  });
+  it('should return false if big', () => {
+    const result = validateCVC('123456789');
+    expect(result).toEqual(false);
+  });
+});
